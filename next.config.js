34109a5f5d3474566279d44feacddc1ae212937a
Yosua Wijaya. <yosuaawijayaaa@gmail.com>
2026-01-06 18:37:14 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 module.exports = nextConfig
